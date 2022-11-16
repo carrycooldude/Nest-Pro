00d4051cd73991a55428a1ab24e861e555f4bf82
carrycooldude <rawatkari554@gmail.com>
2022-11-16 11:35:37 +0530

    Auth Controller

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  login() {
+    return { msg: 'This action means signed in' };
+  }
+
+  signup() {
+    return { msg: 'This action means signed up' };
+  }
+}
